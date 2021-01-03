0a65049b41e01e842f1d700aca4377061d9b07c9
Vincent Salinas <vlsalina@ucsd.edu>
2021-01-03 00:11:04 -0800

    head and nav ejs files added

--- node_project_2/routes/index.js
+++ node_project_2/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Vincent\'s Blog Site', descirption: 'My Blog Site' });
+  res.render('index', { title: 'Vincent\'s Blog Site', description: 'My Blog Site' });
 });
 
 module.exports = router;
